c2d3fd40cedcfa9d1992c5ea0c1382dc0036c06e
cesar <clopez-2019156@kinal.edu.gt>
2021-09-21 16:43:07 -0600

    boton general

--- programa_pedidos/index.php
+++ programa_pedidos/index.php
@@ -216,9 +216,12 @@ while ($fila = mysqli_fetch_array($respuesta_semanas)) {
         <div id="OpcionesPedidoOnline" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Opciones:</h6>
+            <a class="collapse-item" href="./inventarios_php/general.html">
+              <i class="fas fa-receipt"></i>&nbsp General</a>
             <a class="collapse-item" href="./inventarios_php/buscar_pedido_online.html"><i class="fas fa-stream"></i>&nbsp Buscar</a>
             <a class="collapse-item" href="./inventarios_php/agregar_pedido_online.html">
               <i class="fas fa-receipt"></i>&nbsp Agregar</a>
+              
           </div>
         </div>
       </li>
